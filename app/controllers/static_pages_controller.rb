afe5dc9fa38fa68514f32a5bc289a15918aa768d
Nestor Correa <nestor@gelaskins.com>
2013-09-13 09:48:26 -0400

    Final project commit without comments

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,26 +1,41 @@
 class StaticPagesController < ApplicationController
+  # =================================
+  # Imports
+  # =================================
   include ImageMagickTestScriptHelper
+
+
+  # =================================
+  # Properties
+  # =================================
   @wrap_array
+
+
+
+  # =================================
+  # Action Methods
+  # =================================
   def home
     logger.debug 'home called'
   end
 
   def execute_script
-
     logger.debug 'execute_script called'
 
-    @wrap_array = ImageMagickTestScriptHelper.process_image ''
+    source_file_path = "#{Rails.root}/app/assets/images/source.jpg"
+    logger.debug "########loading image from #{source_file_path}"
 
 
-    redirect_to '/view_result'
+    source_image =  Image.read(source_file_path).first
+
+
+    @wrap_array = ImageMagickTestScriptHelper.process_image source_image, "#{Rails.root}/app/assets/images/processed/"
+
+    #redirect_to '/view_result'
   end
 
   def show
 
   end
 
-  def view_result
-    logger.debug 'show called'
-
-  end
 end
--- app/helpers/image_magick_test_script_helper.rb
+++ app/helpers/image_magick_test_script_helper.rb
@@ -1,37 +1,59 @@
 module ImageMagickTestScriptHelper
+=begin
+Explination
+=end
+
+
+  # =================================
+  # Custom Code
+  # =================================
+  def self.run_script
+    save_rmagick_image @source_image, 'exported.jpg'
+  end
+
+
+
+  # ======================================
+  #  |||||||| DO NOT TOUCH BELOW ||||||||
+  # ======================================
+  # =================================
+  # Imports
+  # =================================
   require 'RMagick'
   include Magick
 
 
-  def self.process_image
-    @source_image =  Image.read("#{dropbox_path}wrap_generator/source_images/#{@artwork.artwork_file_name}").first
-    @send_array = Array.new
-    Rails.logger.debug "+++++++++++++"
-    Rails.logger.debug "+++++++++++++"
-    Rails.logger.debug "+++++++++++++"
-    Rails.logger.debug "+++++++++++++"
-    Rails.logger.debug "+++++++++++++"
+  # =================================
+  # Properties
+  # =================================
+  @send_array     # An array of image file names which is then shown on the view
+  @source_image   # The image to be manipulated
+  @export_path    # The location of where to export saved images. Passed from the controller.
+
+
+  # =================================
+  # Methods
+  # =================================
+  def self.process_image(magic_image, export_path)
+    @source_image = magic_image
+    @send_array   = Array.new
+    @export_path  = export_path
 
     run_script
 
     @send_array
   end
 
-  def self.run_script
-
-  end
-
-
-
   # ======================================
   # Save image
   # ======================================
-  def self.save_image(image, name)
-    #logger.debug "Saving #{@wrap_export_path}#{name}"
+  def self.save_rmagick_image(image, name)
+    Rails.logger.debug "Saving #{@export_path}#{name}"
+    image.write("#{@export_path}#{name}")
+    @send_array.push(name)
+  end
 
-    image.write("#{@wrap_export_path}#{name}")
-
-    @send_image_array.push(name)
-
+  def self.add_image_to_return(name)
+    @send_array.push(name)
   end
 end
